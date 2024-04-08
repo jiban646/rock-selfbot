92cbbb036be14edd424c0eb89b7115824f0278ce
jiban646 <166380451+jiban646@users.noreply.github.com>
2024-04-08 14:42:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "Your_Token",
+    token: "",
     prefix: "!",
-    allowedUserIDs: ['yourid1', 'yourid2', 'yourid3']
+    allowedUserIDs: ['1139794777680908318', 'yourid2', 'yourid3']
 };
