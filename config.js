8a70f64b4805b6337834b3b1ee879c6677b15f9e
jiban646 <166380451+jiban646@users.noreply.github.com>
2024-04-14 09:23:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "sJwKehnJ6S7H8IMgycNNSXdD8Spma0VY9CQH48wP",
+    token: "MTIyODkxMzcxNDMzODQ2Nzg1MA.GPBHtU.OK9WODTVzmXwD4-51ItQ1J8wfxv5mF4nkJtC-Y",
     prefix: "!",
     allowedUserIDs: ['1139794777680908318', 'yourid2', 'yourid3']
 };
